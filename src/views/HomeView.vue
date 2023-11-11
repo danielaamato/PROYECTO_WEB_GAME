fcc4bd9777a0840fb10ebe8456feef90a591278e
Daniela Amato Arrieta <daniela.amato@students.salle.url.edu>
2023-11-11 10:32:48 +0100

    SetUp Inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,17 @@ import Menu from '../components/Menu.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
