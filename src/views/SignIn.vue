5e3a3f52533701ca08365435dbea30cd64003d03
Daniela Amato Arrieta <daniela.amato@students.salle.url.edu>
2023-12-14 22:49:25 +0100

    Login y Registro Api

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,23 +2,103 @@
 import "../assets/signin.css";
 export default {
   name: "SignIn",
+
+  data()
+  {
+    return {
+      player_ID: "",
+      password: "",
+    }
+  },
+
+  methods: {
+    async login()
+    {
+      const player =
+          {
+            player_ID: this.player_ID,
+            password: this.password,
+          };
+
+      const result = await this.postUser(player);
+
+      if (result.success)
+      {
+        // Si la respuesta es exitosa, result.response es la respuesta del servidor como texto
+        const { accessToken } = JSON.parse(result.response);
+        localStorage.setItem("token", accessToken);
+        localStorage.setItem("player_ID", player.player_ID);
+        localStorage.setItem("tokenBearer", "Bearer " + localStorage.getItem("token"));
+
+        await this.$router.push({ name: "MenuPrincipal" });
+      }
+      else
+      {
+        // Si hay un error, result.response es el mensaje de error como texto
+        alert("Error: Incorrect username or password");
+      }
+    },
+
+    async postUser(player) {
+      try
+      {
+        const response = await fetch
+        (
+            "https://balandrau.salle.url.edu/i3/players/join",
+            {
+              method: "POST",
+              headers:
+                  {
+                "Content-Type": "application/json",
+              },
+
+              body: JSON.stringify(player),
+            }
+        );
+
+        const responseText = await response.text();
+
+        if (response.status === 200 || response.status === 201)
+        {
+          return { success: true, response: responseText };
+        }
+        else
+        {
+          return { success: false, response: responseText };
+        }
+      }
+      catch (error)
+      {
+        return { success: false, error: error };
+      }
+    },
+  },
 };
+
 </script>
 
 <template>
   <div id="imgbackg2" alt="Background">
       <section class="column1">
-        <form class="signIn">
+        <form class="signIn" @submit.prevent="login">
           <div>
             <h2>Sign in</h2>
           </div>
           <div>
-            <input class="playerid-input" type="text" placeholder="Player ID" name="uname" required />
+            <input class="playerid-input"
+                   type="text"
+                   placeholder="Player ID"
+                   name="uname" required
+                   v-model="player_ID"/>
           </div>
           <div>
-            <input class="password-input" type="password" placeholder="Password" name="psw" required />
+            <input class="password-input"
+                   type="password"
+                   placeholder="Password"
+                   name="psw" required
+                   v-model="password"/>
           </div>
-          <router-link to="/MenuPrincipal" class="signinButtonTwo">Complete</router-link>
+          <button type="submit" class="signinButtonTwo">Complete</button>
           <router-link to="/" class="signinButtonTwo">Back</router-link>
         </form>
       </section>
@@ -26,3 +106,4 @@ export default {
 </template>
 
 <style scoped></style>
+*/
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,19 +1,272 @@
 <script>
 import "../assets/signup.css";
+
 export default {
   name: "SignUp",
+
+  data() {
+    return {
+      player_ID: "",
+      password: "",
+      img: "",
+    };
+  },
+
+  methods: {
+    async signUp()
+    {
+      const newPlayer = {
+        player_ID: this.player_ID,
+        password: this.password,
+        img: this.img,
+      };
+
+      if (this.player_ID === "" || this.password === "" || this.img === "")
+      {
+        alert("Field all fields!");
+      }
+      else if (!this.playerIdAndPasswordValid(this.player_ID))
+      {
+        alert("The player ID must have from 1 to 20 characters!");
+      }
+      else if (!this.playerIdAndPasswordValid(this.password))
+      {
+        alert("The password must have from 1 to 20 characters!");
+      }
+      else
+      {
+        try
+        {
+          const response = await fetch(
+              "https://balandrau.salle.url.edu/i3/players",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newPlayer),
+              }
+          );
+
+          if (response.ok) {
+            const result = await this.postUser(newPlayer);
+
+            if (result)
+            {
+              // Redireccionar a la siguiente página
+              this.$router.push({ name: "MenuPrincipal" });
+            }
+            else
+            {
+              alert(
+                  "Failed to register because this player ID is already registered. Please try again."
+              );
+            }
+          }
+          else
+          {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+          }
+        } catch (error)
+        {
+          console.error(error);
+          alert(
+              "Failed to register because this player ID is already registered. Please try again."
+          );
+        }
+      }
+    },
+
+    async postUser(newPlayer)
+    {
+      try
+      {
+        const response = await fetch(
+            "https://balandrau.salle.url.edu/i3/players/join",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(newPlayer),
+            }
+        );
+
+        if (response.ok)
+        {
+          const responseData = await response.json();
+
+          // Guardar el token y otras operaciones aquí si es necesario
+
+          // Devolver true para indicar que la operación fue exitosa
+          return true;
+        }
+        else
+        {
+          // Manejar errores específicos aquí si es necesario
+          alert("Error while using the API");
+          return false;
+        }
+      }
+      catch (error)
+      {
+        console.error(error);
+        return false;
+      }
+    },
+
+    playerIdAndPasswordValid(field) {
+      const emailRegex = /^.{1,20}$/;
+      return emailRegex.test(field);
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="imgbackg3" alt="Background">
+    <section class="column1">
+      <form class="signUp" @submit.prevent="signUp()">
+        <h2>Sign Up</h2>
+        <input class="imgurl-input"
+               type="text"
+               placeholder="Image Url"
+               name="uname" required
+               v-model="img"/>
+        <input
+            class="playerid-input"
+            type="text"
+            placeholder="Player ID"
+            name="uname" required
+            v-model="player_ID"/>
+        <input
+            class="password-input"
+            type="password"
+            placeholder="Password"
+            name="psw" required
+            v-model="password"/>
+        <button type="submit" class="signupButtonTwo">Complete</button>
+        <router-link to="/" class="signupButtonTwo">Back</router-link>
+      </form>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
+
+
+<!--
+<script>
+import "../assets/signup.css";
+export default
+{
+  name: "SignUp",
+
+  data()
+  {
+    return {
+      player_ID: "",
+      password: "",
+      img: "",
+    };
+  },
+  methods: {
+    async signUp()
+    {
+      const newPlayer = {
+        player_ID: this.player_ID,
+        password: this.password,
+        img: this.img,
+      };
+
+      if (this.player_ID === "" || this.password === "" || this.img === "")
+      {
+        alert("Field all fields!");
+      }
+      else if (!this.playerIdAndPasswordValid(this.player_ID))
+      {
+        alert("The player ID must have from 1 to 20 characters!");
+      }
+      else if (!this.playerIdAndPasswordValid(this.password))
+      {
+        alert("The password must have from 1 to 20 characters!");
+      }
+      else
+      {
+        try
+        {
+          const response = await
+              fetch("https://balandrau.salle.url.edu/i3/players", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newPlayer),
+
+              })
+                  .then((res) => {
+                    console.log("algo");
+                  })
+                  .then((response) =>
+                  {
+                    console.log("algo2");
+                    if (response != null)
+                    {
+                      const result = this.postUser(newPlayer);
+
+                      if (result)
+                      {
+                        console.log("algo3");
+                        this.$router.replace({name: "login"});
+                      }
+                      else
+                      {
+                        alert("Failed to register because this player ID is already registered. Please try again.");
+                      }
+                    }
+                  });
+        }
+        catch (error)
+        {
+          console.error(error);
+          return false;
+        }
+      }
+    },
+    playerIdAndPasswordValid(field)
+    {
+      const emailRegex = /^.{1,20}$/;
+
+      return emailRegex.test(field);
+    },
+  }
 };
 </script>
 
 <template>
   <div id="imgbackg3" alt="Background">
       <section class="column1">
-          <form class="signUp">
+          <form class="signUp" @submit.prevent="signUp()">
               <h2>Sign Up</h2>
-              <input class="imgurl-input" type="text" placeholder="Image Url" name="uname" required />
-              <input class="playerid-input" type="text" placeholder="Player ID" name="uname" required />
-              <input class="password-input" type="password" placeholder="Password" name="psw" required />
-            <router-link to="/MenuPrincipal" class="signupButtonTwo">Complete</router-link>
+              <input class="imgurl-input"
+                     type="text"
+                     placeholder="Image Url"
+                     name="uname" required
+              v-model="img"/>
+              <input
+                  class="playerid-input"
+                  type="text"
+                  placeholder="Player ID"
+                  name="uname" required
+              v-model="player_ID"/>
+              <input
+                  class="password-input"
+                  type="password"
+                  placeholder="Password"
+                  name="psw" required
+              v-model="password"/>
+            <button type="submit" class="signupButtonTwo">Complete</button>
             <router-link to="/" class="signupButtonTwo">Back</router-link>
           </form>
       </section>
@@ -21,3 +274,4 @@ export default {
 </template>
 
 <style scoped></style>
+-->
